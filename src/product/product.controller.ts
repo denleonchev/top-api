2ec35b9f1eb7050d05aab46eb5d4cb357d89e3bd
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-23 16:04:18 +0000

    add product controller draft

---
+++ src/product/product.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ProductModel } from './product.model';
+import { FindProductDto } from './dto/product.dto';
+
+@Controller('product')
+export class ProductController {
+  @Post('create')
+  async create(@Body() dto: Omit<ProductModel, '_id'>) {}
+
+  @Get(':id')
+  async get(@Param('id') id: string) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Patch(':id')
+  async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
+
+  @Get()
+  async find(@Query() query: FindProductDto) {}
+}
